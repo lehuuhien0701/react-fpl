bf29d4234e3ab78769d73e6b56768a087a392f74
lehuuhien0701 <55124459+lehuuhien0701@users.noreply.github.com>
2025-11-08 16:19:43 +0700

    Initial commit - upload project

--- lib/strapi/fetchContentTypeClient.ts
+++ lib/strapi/fetchContentTypeClient.ts
@@ -1,6 +1,6 @@
 import qs from "qs";
 
-const STRAPI_BASE = process.env.NEXT_PUBLIC_STRAPI_API_URL || process.env.STRAPI_API_URL || "http://localhost:1337";
+const STRAPI_BASE = process.env.NEXT_PUBLIC_API_URL || process.env.STRAPI_API_URL || "http://localhost:1337";
 const API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN || process.env.STRAPI_API_TOKEN;
 
 export default async function fetchContentTypeClient(
