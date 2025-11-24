8a22f192f6e78a4bf89a775f8b8eb67e5056cfcc
dtgiang251 <giang251@gmail.com>
2025-11-24 20:55:38 +0700

    add sitemap

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -84,6 +84,7 @@ export default async function LocaleLayout({
 
     return (
         <html lang={currentLocale}>
+            
             <ViewTransitions>
                 <CartProvider>
                     <body
